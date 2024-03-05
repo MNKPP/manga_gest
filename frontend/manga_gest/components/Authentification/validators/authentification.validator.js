8e0f46e4ded9db126013ba182d92b68c767a8a2a
mikegodfrinne <mike.godfrinne.dev@gmail.com>
2024-03-05 14:12:46 +0100

    Added validation message in French

--- frontend/manga_gest/components/Authentification/validators/authentification.validator.js
+++ frontend/manga_gest/components/Authentification/validators/authentification.validator.js
@@ -1,11 +1,25 @@
 import * as yup from "yup";
 
 const registerSchema = yup.object({
-    username: yup.string().min(3).max(20).required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(8).required(),
-    confirm: yup.string().oneOf([yup.ref('password')]).required(),
-}).required()
+    username: yup.string()
+        .min(3, 'Le nom d\'utilisateur doit comporter au moins 3 caractères')
+        .max(20, 'Le nom d\'utilisateur ne peut pas comporter plus de 20 caractères')
+        .required('Le nom d\'utilisateur est obligatoire'),
+
+    email: yup.string()
+        .email('L\'adresse e-mail n\'est pas valide')
+        .required('L\'adresse e-mail est obligatoire'),
+
+    password: yup.string()
+        .min(8, 'Le mot de passe doit comporter au moins 8 caractères')
+        .required('Le mot de passe est obligatoire'),
+
+    confirm: yup.string()
+        .oneOf([yup.ref('password')], 'Les mots de passe ne correspondent pas')
+        .required('La confirmation du mot de passe est obligatoire'),
+
+}).required('Tous les champs sont obligatoires')
+
 
 export {
     registerSchema
