8a15e7b82c8cb6cee88699670bc3f046d6cf375c
mikegodfrinne <mike.godfrinne.dev@gmail.com>
2024-03-11 14:07:31 +0100

    Add toggle functionality to authentication component
    In the Authentication component, a state variable "toggle" has been introduced to switch between the Login and Register components. A button has been added to trigger the state change, hence providing an improved user experience by letting users switch between the login and registration forms on the same page.

--- frontend/manga_gest/components/Authentification/Authentification.jsx
+++ frontend/manga_gest/components/Authentification/Authentification.jsx
@@ -1,11 +1,18 @@
-import { Login, Register} from '../index.js';
+import { Login, Register } from '../index.js';
+import { useState } from "react";
 
 export default function Authentification() {
+    const [toggle, setToggle] = useState(true);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setToggle(!toggle);
+    }
 
     return (
         <section>
-            <Login />
-            <Register />
+            { toggle ? <Login /> : <Register /> }
+            <button onClick={handleSubmit}>{ toggle ? "Se connecter" : "S'inscrire"}</button>
         </section>
     )
 }
