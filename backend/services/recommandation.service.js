387ef445c7d8634f52071386f5221c79b2669bfc
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2024-05-13 14:11:21 +0200

    Update recommendationService with genre mapping and cosine similarity
    Updated the recommendationService in the backend to support mapping of genre names to their respective IDs. Additionally, implemented cosine similarity for more precise anime recommendations. This approach considers not only users' favourite genres but also the similarity of the synopsis to the user's favourite animes.

--- backend/services/recommandation.service.js
+++ backend/services/recommandation.service.js
@@ -1,10 +1,20 @@
 import db from "../models/index.js";
 import axios from "axios";
-import {AnimeDto} from "../dto/anime.dto.js";
+import { AnimeDto } from "../dto/anime.dto.js";
 import natural from 'natural';
+
 const TfIdf = natural.TfIdf;
 
 const recommendationService = {
+    genreIdMap: new Map(),
+
+    loadGenreIds: async () => {
+        const response = await axios.get('https://api.jikan.moe/v4/genres/anime');
+        response.data.data.forEach(genre => {
+            recommendationService.genreIdMap.set(genre.name, genre.mal_id);
+        });
+    },
+
     getUserPreferences: async (userId) => {
         const animeFromFavoriteList = await db.AnimeList.findOne({
             where: {
@@ -15,34 +25,65 @@ const recommendationService = {
                 model: db.Anime,
                 required: true
             }]
-        })
+        });
 
         if (!animeFromFavoriteList) {
             throw new Error("Favorite list not found");
         }
 
-        // map the Animes to reduce the depth
-        return animeFromFavoriteList.Animes.map(anime => anime.dataValues);
+        return animeFromFavoriteList.Animes.map(anime => {
+            return {
+                id: anime.id,
+                genre: anime.genre,
+                synopsis: anime.synopsis
+            };
+        });
     },
 
     getAllAnimeDetails: async (userProfile) => {
-        const preferredGenres = Array.from(userProfile).join(',');
+        await recommendationService.loadGenreIds();
+        const genreIds = Array.from(userProfile).map(genre => recommendationService.genreIdMap.get(genre) || '').filter(id => id !== '');
+        const preferredGenres = genreIds.join(',');
 
-        const animeDetailsResponse = await axios.get(`https://api.jikan.moe/v4/anime?q=&genre=${preferredGenres}&score=8&type=tv`);
+        let allAnimeDetails = [];
+        let currentPage = 1;
+        let totalPages = 19;
 
-        return animeDetailsResponse.data;
+        let requestCount = 0;
+        let startTime = Date.now();
+
+        do {
+            const response = await axios.get(`https://api.jikan.moe/v4/anime?q=&page=${currentPage}&genre=${preferredGenres}&type=tv`);
+            const data = response.data.data;
+            allAnimeDetails = allAnimeDetails.concat(data);
+
+            currentPage++;
+            requestCount++;
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            if (requestCount >= 20) {
+                const elapsedSeconds = (Date.now() - startTime) / 1000;
+                const remainingTime = 60 - elapsedSeconds > 0 ? 60 - elapsedSeconds : 0;
+                await new Promise(resolve => setTimeout(resolve, remainingTime * 1000));
+                requestCount = 0;
+                startTime = Date.now();
+            }
+
+        } while (currentPage <= totalPages);
+
+        return allAnimeDetails;
     },
 
     getUserProfile: async (userId) => {
         const userPreferences = await recommendationService.getUserPreferences(userId);
         const userProfile = new Set();
 
-        for (let anime of userPreferences) {
-            const genres = anime.genre.split(', ');
-            for (let genre of genres) {
+        userPreferences.forEach(anime => {
+            anime.genre.split(', ').forEach(genre => {
                 userProfile.add(genre.trim());
-            }
-        }
+            });
+        });
 
         return userProfile;
     },
@@ -60,40 +101,62 @@ const recommendationService = {
         return score;
     },
 
+    cosineSimilarity: (vec1, vec2) => {
+        const dotProduct = vec1.reduce((acc, current, idx) => acc + (current * vec2[idx]), 0);
+        const magnitude1 = Math.sqrt(vec1.reduce((acc, val) => acc + (val * val), 0));
+        const magnitude2 = Math.sqrt(vec2.reduce((acc, val) => acc + (val * val), 0));
+        return dotProduct / (magnitude1 * magnitude2);
+    },
+
     getRecommendations: async (userId) => {
         const tfidf = new TfIdf();
+        const userPreferences = await recommendationService.getUserPreferences(userId);
+        const allAnimeDetails = await recommendationService.getAllAnimeDetails(await recommendationService.getUserProfile(userId));
 
-        const userProfile = await recommendationService.getUserProfile(userId);
-        const allAnimeDetails = await recommendationService.getAllAnimeDetails(userProfile);
-        const animeList = allAnimeDetails.data;
-
-        // calculate tf-idf
-        animeList.forEach(anime => {
+        userPreferences.forEach(anime => {
             if (anime.synopsis) {
                 tfidf.addDocument(anime.synopsis);
             }
         });
 
-        const scoredAnime = animeList.map(anime => {
-            const animeDto = new AnimeDto(anime);
-            let scoreTfIdf = 0;
-
-            if (animeDto.synopsis) {
-                const terms = tfidf.listTerms(tfidf.documents.length - 1).slice(0, 10); // consider top 10 (can be adjusted) terms only
-                terms.forEach(term => {
-                    scoreTfIdf += term.tfidf;
-                });
+        allAnimeDetails.forEach(anime => {
+            if (anime.synopsis) {
+                tfidf.addDocument(anime.synopsis);
             }
+        });
+
+        const documentVectors = [];
+        for (let i = 0; i < tfidf.documents.length; i++) {
+            documentVectors.push(tfidf.listTerms(i).map(term => term.tfidf));
+        }
+
+        const userProfileVector = documentVectors.slice(0, userPreferences.length).reduce((acc, vec) => {
+            return acc.map((val, idx) => val + (vec[idx] || 0));
+        }, new Array(documentVectors[0].length).fill(0)).map(val => val / userPreferences.length);
+
+        const scoredAnime = (await Promise.all(allAnimeDetails.map(async (anime, index) => {
+            const animeDto = new AnimeDto(anime);
+            let similarityScore = 0;
+            let genreScore = recommendationService.scoreAnime(animeDto, await recommendationService.getUserProfile(userId));
+            let vectorIndex = index + userPreferences.length;
+
+            if (documentVectors[vectorIndex].length > 0) {
+                similarityScore = recommendationService.cosineSimilarity(userProfileVector, documentVectors[vectorIndex]);
+            }
+
+            const totalScore = 0.1 * genreScore + 0.9 * similarityScore;
 
             return {
                 anime: animeDto,
-                score: recommendationService.scoreAnime(animeDto, userProfile) + scoreTfIdf // add score from tf-idf to the previous score
+                score: totalScore,
+                similarityScore
             };
-        });
+        })))
+            .filter(item => item.similarityScore > 0)
+            .sort((a, b) => b.score - a.score)
+            .slice(0, 10);
 
-        scoredAnime.sort((a, b) => b.score - a.score);
-
-        return scoredAnime.slice(0, 10); // return top 10 anime
+        return scoredAnime;
     }
 }
 
